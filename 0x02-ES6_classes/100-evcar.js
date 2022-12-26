d325804799b3ba7b099f3428a20041eb307ee418
samueltezera28 <samueltezera28@gmail.com>
2022-12-26 09:29:49 +0300

    EVCar Update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,23 +1,14 @@
+
+/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    // call constructor of super class (Building)
-    super(brand, motor, color);
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-    // Create objs
-    this._range = range;
-  }
-
-  // Methods
-
-  cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
-  }
-
-  // Setters
-
-  // Getters
+	cloneCar() {
+		return new Car();
+	}
 }
